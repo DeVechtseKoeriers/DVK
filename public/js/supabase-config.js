69e59bea7b1b454587d38f4186931d1eba186e18
DeVechtseKoeriers <devechtsekoeriers@gmail.com>
2026-02-13 10:18:59 +0200

    Update supabase-config.js

--- public/js/supabase-config.js
+++ public/js/supabase-config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://awjmjhwfjgvcbpnabzes.supabase.co";
+const SUPABASE_URL = "https://awjmjhvfigvcbpnabzes.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3am1qaHZmaWd2Y2JwbmFiemVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5MTY3MDYsImV4cCI6MjA4NjQ5MjcwNn0.As9vdyAGs1elV72X1iPnmTZTs17JTRQBeFfR8qPYg2s";
 
 window.supabaseClient = window.supabase.createClient(
